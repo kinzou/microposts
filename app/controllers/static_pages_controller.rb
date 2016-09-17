891c4b3ddf1254fbb304923a224fe0ee4c83227d
kinzou <kinzou59@gmail.com>
2016-09-17 20:03:15 +0000

    完了

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+<<<<<<<<< saved version
      @micropost = current_user.microposts.build if logged_in?
   end
+=========
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+    end
+  ends
+>>>>>>>>> local version
 end
